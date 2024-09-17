0b0d3a2318b0053948b8d60ca41d9be78be5297e
Mark Carson <markcarson121@gmail.com>
2024-09-17 15:17:55 -0300

    created masonry effect

---
+++ src/app/demos/cloudinary-masonry/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Image from "next/image";
+
+import { cloudinaryLoader } from "@/app/functions/cloudinaryLoader";
+
+export default function CloudinaryMasonry() {
+  const imageUrls = [
+    "/textures/texture-one",
+    "/textures/texture-two",
+    "/textures/texture-three",
+    "/textures/texture-four",
+    "/textures/texture-five",
+  ];
+
+  return (
+    <main className="mx-auto max-w-[900px] columns-3 space-y-4 pt-4">
+      {imageUrls.map((src, index) => {
+        return (
+          <Image
+            className="h-auto w-[400px]"
+            src={src}
+            alt={`Texture-${index + 1}`}
+            priority
+            width={300}
+            height={600}
+            loader={cloudinaryLoader}
+          />
+        );
+      })}
+    </main>
+  );
+}
